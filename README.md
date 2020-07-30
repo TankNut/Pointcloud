f6cb5c80356d570e4351d65488aa647d10229cae
TankNut <m.vlasblom@live.nl>
2020-07-30 02:47:45 +0200

    Massive simplifications
    - The map now always uses the vertical slice method, and the other method has been removed together with the associated console variable. The performance impact didn't seem major enough to warrant keeping it around
    - Points are now just drawn to the rendertargets directly instead of using an orthographic camera
    - Massively simplified the math involved with calculating the required map offset

--- README.md
+++ README.md
@@ -4,9 +4,8 @@ A work in progress minimap addon based around the idea of using a point cloud to
 ## Console variables
 - `pointcloud_enabled` - Whether or not to enable the mapper and minimap
 - `pointcloud_resolution` - How many units one point represents
-- `pointcloud_scale` - **Currently broken, do not change**
+- `pointcloud_scale` - **Doesn't do anything for now**
 
 - `pointcloud_minimap_width` - How wide the minimap should be in pixels
 - `pointcloud_minimap_tall` - How tall the minimap should be in pixels
 - `pointcloud_minimap_zoom` - Self-explanatory
-- `pointcloud_minimap_slices` - Whether or not to enable vertical slicing, may cause performance issues depending on the map and resolution used
--- lua/autorun/client/cl_pointcloud.lua
+++ lua/autorun/client/cl_pointcloud.lua
@@ -6,7 +6,6 @@ pointcloud = {
 	MapWidth = CreateClientConVar("pointcloud_minimap_width", "300", true, false, "How wide the minimap display should be", 0),
 	MapHeight = CreateClientConVar("pointcloud_minimap_height", "300", true, false, "How tall the minimap display should be", 0),
 	MapZoom = CreateClientConVar("pointcloud_minimap_zoom", "1", true, false, "How far to zoom in on the minimap"),
-	MapSlices = CreateClientConVar("pointcloud_minimap_slices", "0", true, false, "Whether or not the minimap should perform vertical slicing"),
 
 	-- Internals
 	Index = pointcloud and pointcloud.Index or 0,
@@ -47,7 +46,6 @@ local length = Vector(1, 1, 1):Length()
 
 function pointcloud:AddPoint(pos, col, sky)
 	local resolution = self.Resolution:GetInt()
-	local slices = self.MapSlices:GetBool()
 
 	pos = pos * (1 / resolution)
 
@@ -66,18 +64,16 @@ function pointcloud:AddPoint(pos, col, sky)
 	self.Points[tostring(pos)] = true
 
 	if not sky and col:Length() <= length then
-		if slices then
-			local rendertarget = self.RenderTargets[slice]
+		local rendertarget = self.RenderTargets[slice]
 
-			if not rendertarget then
-				rendertarget = GetRenderTarget("pointcloud" .. slice, 1024, 1024, true)
+		if not rendertarget then
+			rendertarget = GetRenderTarget("pointcloud" .. slice, 1024, 1024, true)
 
-				self.RenderTargets[slice] = rendertarget
+			self.RenderTargets[slice] = rendertarget
 
-				render.PushRenderTarget(rendertarget)
-					render.Clear(0, 0, 0, 0, true, true)
-				render.PopRenderTarget()
-			end
+			render.PushRenderTarget(rendertarget)
+				render.Clear(0, 0, 0, 0, true, true)
+			render.PopRenderTarget()
 		end
 
 		self.PointList[#self.PointList + 1] = {pos, col}
@@ -126,154 +122,86 @@ hook.Add("PreDrawHUD", "pointcloud", function()
 	end
 
 	local resolution = pointcloud.Resolution:GetFloat()
-	local scale = pointcloud.Scale:GetFloat()
-	local slices = pointcloud.MapSlices:GetBool()
 
 	if #pointcloud.PointList > 0 then
-		local mins, maxs = game.GetWorld():GetModelBounds()
-		local bounds = math.ceil(math.max(-mins.x, -mins.y, maxs.x, maxs.y) * scale) + 10
+		local baseslice = math.Round(lpos.z * (1 / resolution))
 
-		local data = {
-			drawviewer = true,
-			origin = Vector(0, 0, 0),
-			angles = Angle(90, 0, 0),
-			znear = -1000,
-			zfar = 1000,
-			ortho = {
-				left = -bounds,
-				right = bounds,
-				bottom = bounds,
-				top = -bounds
-			}
-		}
+		local i = 0
 
-		cam.Start(data)
-			local boxsize = resolution * scale * 0.5
-
-			local boxmin = Vector(-boxsize, -boxsize, -boxsize)
-			local boxmax = Vector(boxsize, boxsize, boxsize)
-
-			local baseslice = math.Round(lpos.z * (1 / resolution))
-
-			render.SetColorMaterial()
-
-			render.OverrideDepthEnable(true, true)
-			render.OverrideAlphaWriteEnable(true, true)
-
-			if slices then
-				local i = 0
-
-				repeat
-					if pointcloud.Index == #pointcloud.PointList then
-						break
-					end
-
-					pointcloud.Index = pointcloud.Index + 1
-
-					local vec = pointcloud.PointList[pointcloud.Index][1]
-					local col = pointcloud.PointList[pointcloud.Index][2]:ToColor()
-
-					local slice = vec.z * (1 / resolution)
-
-					local rendertarget = pointcloud.RenderTargets[slice]
-
-					render.PushRenderTarget(rendertarget)
-						render.DrawBox(vec * scale, Angle(), boxmin, boxmax, col)
-					render.PopRenderTarget()
-
-					i = i + 1
-				until i == 2048
-			else
-				render.PushRenderTarget(pointcloud.RenderTarget)
-					local i = 0
-
-					repeat
-						if pointcloud.Index == #pointcloud.PointList then
-							break
-						end
-
-						pointcloud.Index = pointcloud.Index + 1
-
-						local vec = pointcloud.PointList[pointcloud.Index][1]
-						local col = pointcloud.PointList[pointcloud.Index][2]:ToColor()
-
-						render.DrawBox(vec * scale, Angle(), boxmin, boxmax, col)
-
-						i = i + 1
-					until i == 2048
-				render.PopRenderTarget()
-
-				pointcloud.Material:SetTexture("$basetexture", pointcloud.RenderTarget)
+		repeat
+			if pointcloud.Index == #pointcloud.PointList then
+				break
 			end
 
-			render.OverrideDepthEnable(false)
-			render.OverrideAlphaWriteEnable(false)
+			pointcloud.Index = pointcloud.Index + 1
 
-			local width = pointcloud.MapWidth:GetInt()
-			local height = pointcloud.MapHeight:GetInt()
-			local zoom = pointcloud.MapZoom:GetFloat()
+			local vec = pointcloud.PointList[pointcloud.Index][1] * (1 / resolution)
+			local col = pointcloud.PointList[pointcloud.Index][2]:ToColor()
 
-			local pos = lpos * scale
-			local default = 1024
+			local rendertarget = pointcloud.RenderTargets[vec.z]
 
-			pos.x = math.Remap(pos.x, -bounds, bounds, -default * 0.5, default * 0.5)
-			pos.y = math.Remap(pos.y, -bounds, bounds, -default * 0.5, default * 0.5)
+			render.PushRenderTarget(rendertarget)
+				cam.Start2D()
+					surface.SetDrawColor(col)
+					surface.DrawRect(-vec.y + 512, -vec.x + 512, 1, 1)
+				cam.End2D()
+			render.PopRenderTarget()
 
-			-- See: https://wiki.facepunch.com/gmod/surface.DrawTexturedRectUV
-			local adjustment = 0.5 / 16
+			i = i + 1
+		until i == 2048
 
-			pos.x = (pos.x - adjustment) / (1 - 2 * adjustment)
-			pos.y = (pos.y - adjustment) / (1 - 2 * adjustment)
+		local width = pointcloud.MapWidth:GetInt()
+		local height = pointcloud.MapHeight:GetInt()
+		local zoom = pointcloud.MapZoom:GetFloat()
 
-			local size = default * zoom
+		local pos = lpos * (1 / resolution)
+		local size = 1024 * zoom
 
-			local x = (width * 0.5) - (size * 0.5)
-			local y = (height * 0.5) - (size * 0.5)
+		-- See: https://wiki.facepunch.com/gmod/surface.DrawTexturedRectUV
+		local adjustment = 0.5 / 16
 
-			cam.Start2D()
-				surface.SetDrawColor(30, 30, 30)
-				surface.DrawRect(0, 0, width, height)
+		pos.x = (pos.x - adjustment) / (1 - 2 * adjustment)
+		pos.y = (pos.y - adjustment) / (1 - 2 * adjustment)
 
-				render.SetStencilWriteMask(0xFF)
-				render.SetStencilTestMask(0xFF)
-				render.SetStencilCompareFunction(STENCIL_ALWAYS)
-				render.SetStencilPassOperation(STENCIL_KEEP)
-				render.SetStencilFailOperation(STENCIL_KEEP)
-				render.SetStencilZFailOperation(STENCIL_KEEP)
+		pos:Mul(zoom)
 
-				render.SetStencilEnable(true)
+		cam.Start2D()
+			surface.SetDrawColor(30, 30, 30)
+			surface.DrawRect(0, 0, width, height)
 
-				render.ClearStencil()
+			render.SetStencilWriteMask(0xFF)
+			render.SetStencilTestMask(0xFF)
+			render.SetStencilCompareFunction(STENCIL_ALWAYS)
+			render.SetStencilPassOperation(STENCIL_KEEP)
+			render.SetStencilFailOperation(STENCIL_KEEP)
+			render.SetStencilZFailOperation(STENCIL_KEEP)
 
-				render.SetStencilReferenceValue(1)
+			render.SetStencilEnable(true)
 
-				render.ClearStencilBufferRectangle(0, 0, width, height, 1)
+			render.ClearStencil()
 
-				render.SetStencilCompareFunction(STENCIL_EQUAL)
+			render.SetStencilReferenceValue(1)
 
-				if slices then
-					for k, v in SortedPairs(pointcloud.RenderTargets) do
-						if k > baseslice then
-							continue
-						end
+			render.ClearStencilBufferRectangle(0, 0, width, height, 1)
 
-						pointcloud.Material:SetTexture("$basetexture", v)
+			render.SetStencilCompareFunction(STENCIL_EQUAL)
 
-						surface.SetDrawColor(255, 255, 255)
-						surface.SetMaterial(pointcloud.Material)
-						surface.DrawTexturedRect(x + (pos.y * zoom), y + (pos.x * zoom), size, size)
-					end
-				else
-					surface.SetDrawColor(255, 255, 255)
-					surface.SetMaterial(pointcloud.Material)
-					surface.DrawTexturedRect(x + (pos.y * zoom), y + (pos.x * zoom), size, size)
+			for k, v in SortedPairs(pointcloud.RenderTargets) do
+				if k > baseslice then
+					continue
 				end
 
-				render.SetStencilEnable(false)
+				pointcloud.Material:SetTexture("$basetexture", v)
 
-				surface.SetDrawColor(255, 0, 0)
-				surface.DrawRect((width * 0.5) - 2, (height * 0.5) - 2, 4, 4)
-			cam.End2D()
-		cam.End()
+				surface.SetDrawColor(255, 255, 255)
+				surface.SetMaterial(pointcloud.Material)
+				surface.DrawTexturedRect((width * 0.5) - (size * 0.5) + pos.y - 2, (height * 0.5) - (size * 0.5) + pos.x - 2, size, size)
+			end
+
+			render.SetStencilEnable(false)
+
+			surface.SetDrawColor(255, 0, 0)
+			surface.DrawRect((width * 0.5) - 2, (height * 0.5) - 2, 4, 4)
+		cam.End2D()
 	end
 end)
