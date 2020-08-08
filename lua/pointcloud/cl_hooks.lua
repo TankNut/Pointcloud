28933409bd61715bc549567d5038a4c0ea338704
TankNut <m.vlasblom@live.nl>
2020-08-08 19:31:02 +0200

    Improved the way enabling/disabling the entire addon is handled

--- lua/pointcloud/cl_hooks.lua
+++ lua/pointcloud/cl_hooks.lua
@@ -34,8 +34,28 @@ hook.Add("PreDrawViewModels", "pointcloud", function()
 end)
 
 hook.Add("PostGamemodeLoaded", "pointcloud", function()
-	pointcloud.Persistence:StartLoader()
+	if pointcloud.Enabled:GetBool() then
+		pointcloud.Persistence:StartLoader()
+	end
 end)
+
+local function enable(name, old, new)
+	if old == new then
+		return
+	end
+
+	if tobool(new) then
+		pointcloud.Persistence:StartLoader()
+	else
+		if pointcloud.Peristence:IsLoading() then
+			pointcloud:FinishLoading()
+		else
+			pointcloud.Persistence:Save()
+		end
+
+		pointcloud:Clear()
+	end
+end
 
 local function clearall(name, old, new)
 	pointcloud.Persistence:Save(tonumber(old))
@@ -52,6 +72,7 @@ local function clearprojection()
 	projection.DrawIndex = 0
 end
 
+cvars.AddChangeCallback("pointcloud_enabled", enable, "pointcloud")
 cvars.AddChangeCallback("pointcloud_resolution", clearall, "pointcloud")
 cvars.AddChangeCallback("pointcloud_projection_mode", clearprojection, "pointcloud")
 cvars.AddChangeCallback("pointcloud_projection_scale", clearprojection, "pointcloud")
