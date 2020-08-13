0969736a226fd0d8e7008418ea01151338811960
TankNut <m.vlasblom@live.nl>
2020-08-13 15:23:24 +0200

    Auto-set workshop changelog

--- workshop.bat
+++ workshop.bat
@@ -4,7 +4,7 @@ cd /d "%~dp0"
 cd ..\..\..\bin\
 
 gmad create -folder "%~dp0\" -out "%~dp0..\__TEMP.gma"
-gmpublish update -addon "%~dp0..\__TEMP.gma" -id "%WORKSHOP_ID%"
+gmpublish update -addon "%~dp0..\__TEMP.gma" -id "%WORKSHOP_ID%" -changes "Changelog provided in-game"
 
 cd /d "%~dp0"
 
